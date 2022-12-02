031c00a2604ecd661555be5b93eed444710612a3
Marek Mokrzycki <marek.mokrzyc7@gmail.com>
2022-12-02 13:22:12 +0100

    3rd version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Name";
+        int myAge = 1;
+        double height = 0.00;
+        char carClass = '?';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Marek";
+        String favoritePen = "Lamy Safari";
+        int myAge = 30;
+        double height = 1.8;
+        char favoriteNibSize = 'F';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(favoritePen);
+        System.out.println(favoriteNibSize);
+        System.out.println(isGoodProgrammer);
+    }
+}
