332c888edf9e59bb43e100006776548a0d311d6e
Marek Mokrzycki <marek.mokrzyc7@gmail.com>
2023-02-14 13:38:21 +0100

    19th version - Tablice i losowanie

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -0,0 +1,25 @@
+package com.kodilla.collections.arrays.homework;
+
+import com.kodilla.collections.interfaces.homework.Cadillac;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Lamborghini;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+public class CarUtils {
+    public static void describeCar (Car car) {
+        System.out.println("------------------------------");
+        System.out.println("Car brand: " + getCarInfo(car));
+        System.out.println("Car's acceleration: " + car.getSpeed());
+    }
+
+    private static String getCarInfo (Car car) {
+        if (car instanceof Cadillac)
+            return "Cadillac";
+        else if (car instanceof Lamborghini)
+            return "Lamborghini";
+        else if (car instanceof Opel)
+            return "Opel";
+        else
+            return "Unknown car brand";
+    }
+}
