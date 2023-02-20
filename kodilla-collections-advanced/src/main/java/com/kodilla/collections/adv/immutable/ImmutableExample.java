0ed1236b08ea667f0bd1fcbb8eab9d9829144a91
Marek Mokrzycki <marek.mokrzyc7@gmail.com>
2023-02-20 18:33:41 +0100

    24th version - Niemutowalność

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable;
+
+public class ImmutableExample {
+    public static void main(String[] args) {
+        Book book = new Book("Jules Verne", "W 80 dni dookoła świata");
+        BookRecord bookRecord = new BookRecord("Frank Herbert", "Heretycy Diuny", 2020);
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    public static void main(String[] args) {
+        TaskRecord somethingToDo = new TaskRecord("Jedzenie", 30);
+        TaskRecord someElseToDo = new TaskRecord("Sprzątanie", 120);
+    }
+}
