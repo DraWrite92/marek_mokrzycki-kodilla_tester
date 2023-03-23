3c33bc819704e536fee540cb210ca2060c144da5
Marek Mokrzycki <marek.mokrzyc7@gmail.com>
2023-03-23 12:59:43 +0100

    26th version - Poprawione pętle

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,45 +1,41 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class RandomNumbers {
-    int result = 0;
     int sum = 0;
+    private int minimum;
+    private int maximum;
 
-    public int getRandoms() {
+    Random random = new Random();
+    public void getRandoms() {
+        this.minimum = 0;
+        this.maximum = 30;
         while (sum <= 5000) {
-            int random = (int) (Math.random() * 30 + 1);
-            sum += random;
-            System.out.println(random);
-            result = random;
+            int number = random.nextInt(31);
+            sum += number;
+            System.out.println(number);
+            if(number <= minimum) {
+                this.minimum = number;
+            }
+            if(number >= maximum) {
+                this.maximum = number;
+            }
         }
         System.out.println("The sum in this instance is " + sum + ".");
-        return sum;
     }
 
-    Scanner sk = new Scanner(System.in);
-    int n = sk.nextInt();
-    int largest = Integer.MAX_VALUE;
-    int smallest = Integer.MIN_VALUE;
-
     public int getLargest() {
-        for (int i = 0; i < n; i++) {
-            int scanned = sk.nextInt();
-            if (scanned > largest) {
-                largest = scanned;
-            }
-            System.out.println("The largest random number is " + largest + ".");
-        }
-        return largest;
+        return maximum;
     }
 
 
     public int getSmallest() {
-        for (int i = 0; i < n; i++) {
-            int scanned = sk.nextInt();
-            if (scanned < smallest) {
-                smallest = scanned;
-            }
-            System.out.println("The smallest random number is " + smallest + ".");
-        }
-        return smallest;
+        return minimum;
+    }
+
+    public static void main(String[] args) {
+        RandomNumbers theRandomizer = new RandomNumbers();
+        theRandomizer.getRandoms();
+        System.out.println("The minimum number in the random sequence is: " + theRandomizer.getSmallest());
+        System.out.println("The maximum number in the random sequence is: " + theRandomizer.getLargest());
     }
 }
